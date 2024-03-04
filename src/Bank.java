e840ececfb95fa4435483e66ed9fcd53485b5ee8
Narasimha6703 <narasimha6703@gmail.com>
2024-03-04 15:42:44 +0530

    Added Bank Class and created a Constructor

---
+++ src/Bank.java
@@ -0,0 +1,16 @@
+public class Bank {
+    String name;
+    int amount;
+    int accountNo;
+    public Bank(String name,int amount,int accountNo){
+        this.name=name;
+        this.accountNo=accountNo;
+        this.amount=amount;
+    }
+
+    //public void Deposit(){}
+
+
+    //public int Balance(){}
+
+}
